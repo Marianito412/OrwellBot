c88693f51c66037ad7e7a6c76cdf59ea6507d87d
Marianito412 <31678416+Marianito412@users.noreply.github.com>
2021-06-17 12:07:02 -0600

    Single notification per event
    Implemented functions and checks to make sure we only notify on the 
    first hour of every event.

--- NotionScheduleNotify.py
+++ NotionScheduleNotify.py
@@ -3,7 +3,7 @@ from notion.client import NotionClient
 import requests
 import pytz
 import os
-    
+
 token = os.environ.get("TOKEN")
 url = os.environ.get("NOTION_URL")
 
@@ -16,18 +16,24 @@ def sendMessage(msg, eventStart, eventEnd):
 
     requests.get(url, params={'chat_id':'1045229863','text': msg, 'parse_mode':'HTML'})
 
+def isBetween(Start: str, End: str, format="%I:%M %p") -> bool:
+    """This function returns true if the current time exists within the given times"""
+    tz = pytz.timezone("America/Costa_Rica")
+    now = datetime.now(tz)
+    return (now.time() >= datetime.strptime(Start, format).time()) and (now.time() <= datetime.strptime(End, format).time())
+
 def main():
     tz = pytz.timezone("America/Costa_Rica")
     now = datetime.now(tz)
     print(now.weekday(), now.day)
 
-    isEarly = (now.time() >= datetime.strptime("07:00 AM", "%I:%M %p").time()) and (now.time() <= datetime.strptime("08:00 AM", "%I:%M %p").time())
+    isEarly = isBetween("07:00 AM", "08:00 PM")
 
     if (now.weekday() == 3) and  isEarly:
-        sendMessage("**New Free Game in Epic Games Store, Grab It**", "", "")
-    
+        sendMessage("New Free Game in Epic Games Store, Grab It", "", "")
+
     if (now.weekday() == 1 and now.day <=7) and isEarly:
-        sendMessage("**New Unreal Engine MarketPlace Content, Check It Out**", "", "")
+        sendMessage("New Unreal Engine MarketPlace Content, Check It Out", "", "")
 
     client = NotionClient(token_v2=token)
     database = client.get_collection_view(url)
@@ -41,7 +47,10 @@ def main():
         print(f"{eventStart} - {eventEnd}")
 
         if (now.time()>=eventStart and now.time()<=eventEnd) and (now.strftime("%A") in i.Days):
-            sendMessage(i.name, eventStart.strftime("%I:%M %p"), eventEnd.strftime("%I:%M %p"))
+
+            firstHour = [x.replace("md", "pm").upper() for x in Hours[0].split("→")]
+            if isBetween(firstHour[0], firstHour[1], format="%I:%M%p"):
+                sendMessage(i.name, eventStart.strftime("%I:%M %p"), eventEnd.strftime("%I:%M %p"))
 
 if __name__ == "__main__":
     main()
