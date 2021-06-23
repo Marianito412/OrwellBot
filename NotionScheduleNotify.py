3a833fcd1ded39869ea7213087d7ec66b36b1e70
Marianito412 <31678416+Marianito412@users.noreply.github.com>
2021-06-23 15:46:30 -0600

    Moved to the official Notion API

--- NotionScheduleNotify.py
+++ NotionScheduleNotify.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-from notion.client import NotionClient
-import requests
+import requests, json
 import pytz
 import os
 
@@ -8,20 +7,42 @@ DEBUG = False
 
 if DEBUG:
     import keys
-
-    token = keys.Token
-    url = keys.url
     BOT_API_KEY = keys.orwellKey
+
+    NotionKey = keys.NotionSecret
+    databaseId = keys.NotionDatabaseId
 else:
-    token = os.environ.get("TOKEN")
-    url = os.environ.get("NOTION_URL")
     BOT_API_KEY = os.environ.get("ORWELL_API")
+
+    NotionKey = os.environ.get("NOTION_KEY")
+    databaseId = os.environ.get("DATABASE_ID")
+
+header = {
+    "Authorization": f"Bearer {NotionKey}",
+    "Notion-Version": "2021-05-13",
+    "Content-Type": "application/json",
+}
+
+class event:
+    Name = None
+    Days = None
+    Hours = None
+    def __init__(self, package):
+        self.Name = package["properties"]["Name"]["title"][0]["text"]["content"]
+        self.Days = [Day["name"] for Day in package["properties"]["Days"]["multi_select"]]
+        self.Hours = [Hour["name"] for Hour in package["properties"]["Hours"]["multi_select"]]
+
 
 def sendMessage(msg, eventStart, eventEnd):
     url = 'https://api.telegram.org/bot{}/sendMessage'.format(BOT_API_KEY)
     msg = f"<b>{msg}</b>\n{eventStart}-{eventEnd}"
 
     requests.get(url, params={'chat_id':'1045229863','text': msg, 'parse_mode':'HTML'})
+
+def getSchedule(databaseId, headers):
+    url = f"https://api.notion.com/v1/databases/{databaseId}/query"
+
+    return requests.request("POST", url, headers=headers).json()
 
 def isBetween(Start: str, End: str, format="%I:%M %p") -> bool:
     """This function returns true if the current time exists within the given times"""
@@ -41,10 +62,9 @@ def main():
     if (now.weekday() == 1 and now.day <=7) and isEarly:
         sendMessage("New Unreal Engine MarketPlace Content, Check It Out", "", "")
 
-    client = NotionClient(token_v2=token)
-    database = client.get_collection_view(url)
+    events = [event(item) for item in getSchedule(databaseId, header).get("results")]
 
-    for i in database.collection.get_rows():
+    for i in events:
         eventStart = i.Hours[0].split("→")[0].replace("md", "pm").upper()
         eventStart = datetime.strptime(eventStart, "%I:%M%p").time()
 
@@ -56,7 +76,9 @@ def main():
 
             firstHour = [x.replace("md", "pm").upper() for x in i.Hours[0].split("→")]
             if isBetween(firstHour[0], firstHour[-1], format="%I:%M%p"):
-                sendMessage(i.name, eventStart.strftime("%I:%M %p"), eventEnd.strftime("%I:%M %p"))
+                sendMessage(i.Name, eventStart.strftime("%I:%M %p"), eventEnd.strftime("%I:%M %p"))
 
 if __name__ == "__main__":
     main()
+    #with open("test.json", "w", encoding="utf-8") as f:
+    #    json.dump(getSchedule(databaseId, header), f, ensure_ascii=False)
